af28a43b32a8b7b53d13fb6d7546956c51a5ad8a
Jasonn Chang <changjason96@gmail.com>
2025-05-20 23:44:05 -0400

    removed modules that weren't used

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const {fetchMyIP} = require('./iss');
-const {fetchCoordsByIP} = require('./iss');
-const {fetchISSFlyOverTimes} = require('./iss');
 const {nextISSTimesForMyLocation} = require('./iss');
 
 const printPasstimes = function (passTimes) {
